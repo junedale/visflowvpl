d16f6e5ca843552309967db9bd1320e3c66aaafd
junedale <55942587+junedale@users.noreply.github.com>
2023-03-26 13:54:53 +0800

    Delete src directory

--- src/electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-
-contextBridge.exposeInMainWorld("VisFlow", {
-    openDir: () => ipcRenderer.invoke("openDirRequest"),
-    newFile: (fileName, data) => ipcRenderer.invoke("newFileRequest", fileName, data),
-    openFile: (fileName) => ipcRenderer.invoke("openFileRequest", fileName),
-    saveFile: (fileName, data) => ipcRenderer.send("saveFileRequest", fileName, data)
-});
